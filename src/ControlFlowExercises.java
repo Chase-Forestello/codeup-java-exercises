8415404465363fb5f97d79f7fc9842c4c45e7e9d
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-02 15:10:53 -0500

    Incomplete - Java I - Control Flow Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,9 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 0;
+        long i = 2;
         do {
-            System.out.print(i + " ");
-            i+=2;
-        } while (i <= 100);
+            System.out.println(i);
+            i = i*i;
+        } while (i < 1000000);
     }
 }
-
