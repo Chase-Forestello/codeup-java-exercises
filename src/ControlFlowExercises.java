ed85a899017dd40b27bb81334d1593e1908efbb7
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-02 15:36:28 -0500

    Incomplete - Java I - Control Flow Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,7 +1,55 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (long i = 2; i < 1000000 ; i*=i) {
-            System.out.println(i);
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.print(i + " ");
+//            i++;}
+
+//        int i = 0;
+//        do {
+//            System.out.print(i + " ");
+//            i+=2;
+//        } while (i <= 100);
+
+//            int i = 100;
+//            do {
+//                System.out.print(i + " ");
+//                i-=5;
+//            } while (i >= -10);
+
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i = i*i;
+//        } while (i < 1000000);
+
+//        for (int i = 5; i <= 15 ; i++) {
+//            System.out.print(i + " ");
+//        }
+
+//        for (int i = 0; i <= 100 ; i+=2) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 100; i >= -10 ; i-=5) {
+//            System.out.print(i + " ");
+//        }
+
+//        for (long i = 2; i < 1000000 ; i*=i) {
+//            System.out.println(i);
+//        }
+
+        for (int i = 0; i <=100 ; i++) {
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.print("FizzBuzz, ");
+            }
+            else if (i % 3 == 0){
+                System.out.print("Fizz, ");
+            } else if (i % 5 == 0){
+                System.out.print("Buzz, ");
+            } else {
+                System.out.print(i + ", ");
+            }
         }
     }
 }
