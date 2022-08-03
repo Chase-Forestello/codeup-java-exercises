f6fa1927f5b3eb2d44dee32c1968d3526c321f44
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-03 11:35:49 -0500

    Complete - Java I - Console IO Exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,31 +1,30 @@
 // Complete - Java I - Console IO Exercises
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
 public class ConsoleExercises {
     public static void main(String[] args) {
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately" + " %.2f%n", pi);
-//
-        Scanner s = new Scanner(System.in);
-//        System.out.print("Enter an integer: ");
-//        int userInput = s.nextInt();
-//
-//        System.out.print("Enter 3 words: ");
-//        String userInput1 = s.next();
-//        String userInput2 = s.next();
-//        String userInput3 = s.next();
-//        System.out.printf("%s%n%s%n%s%n", userInput1, userInput2, userInput3);
-//
-//        s.nextLine();
-//
-//        System.out.print("Enter a sentence:");
-//        String userSentence = s.nextLine();
-//        System.out.printf("%s%n", userSentence);
-//
-//        s.nextLine();
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately" + " %.2f%n", pi);
 
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int userInput = s.nextInt();
+
+        s.nextLine();
+
+        System.out.print("Enter 3 words: ");
+        String userInput1 = s.next();
+        String userInput2 = s.next();
+        String userInput3 = s.next();
+        System.out.printf("%s%n%s%n%s%n", userInput1, userInput2, userInput3);
+
+        s.nextLine();
+
+        System.out.print("Enter a sentence:");
+        String userSentence = s.nextLine();
+        System.out.printf("%s", userSentence);
+
+//        s.nextLine();
 //        System.out.print("Enter a length:");
 //        String userLength = s.nextLine();
 //        System.out.println("Length: " + userLength + " ft");
@@ -38,13 +37,11 @@ public class ConsoleExercises {
 //        int perimeter = (userLengthParsed * 2) + (userWidthParsed * 2);
 //        System.out.printf("Area: %s sqft%nPerimeter: %s ft%n", area, perimeter);
 
-//        s.nextLine();
-        s.useDelimiter("/n");
+        s.nextLine();
+//        s.useDelimiter("/n");
         System.out.print("Enter a length, width, and height:");
         float userLength = s.nextFloat();
-        s.nextLine();
         float userWidth = s.nextFloat();
-        s.nextLine();
         float userHeight = s.nextFloat();
         float area = userLength * userWidth;
         float perimeter = (userLength * 2) + (userWidth * 2);
