b8133d402e457e6fb74c2aad43216238cbb18ba1
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-03 09:56:48 -0500

    Java I - Java Intro - Hello World Exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.Integer.parseInt;
+
 public class HelloWorld {
     public static void main(String[] args) {
         // Hello World with and without new line
@@ -34,61 +36,5 @@ public class HelloWorld {
         System.out.println(y2);
     }
 }
-class ConsoleExercises {
-    public static void main(String[] args) {
-        double pi = 3.14159;
-        System.out.format("The value of pi is approximately" + " %.2f%n", pi);
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter an integer: ");
-        int userInput = s.nextInt();
-
-        System.out.print("Enter 3 words: ");
-        String userInput1 = s.next();
-        String userInput2 = s.next();
-        String userInput3 = s.next();
-        System.out.printf("%s%n%s%n%s%n", userInput1, userInput2, userInput3);
-
-        s.nextLine();
-
-        System.out.print("Enter a sentence:");
-        String userSentence = s.nextLine();
-        System.out.printf("%s%n", userSentence);
-
-        s.close();
-    }
-}
-class CodeupPerimeter {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter a length:");
-        String userLength = s.nextLine();
-        System.out.println("Length: " + userLength + " ft");
-        System.out.print("Enter a width:");
-        String userWidth = s.nextLine();
-        System.out.println("Width: " + userWidth + " ft");
-        int userLengthParsed = Integer.parseInt(userLength);
-        int userWidthParsed = Integer.parseInt(userWidth);
-        int area = userLengthParsed * userWidthParsed;
-        int perimeter = (userLengthParsed * 2) + (userWidthParsed * 2);
-        System.out.printf("Area: %s sqft%nPerimeter: %s ft%n", area, perimeter);
-        s.close();
-    }
-}
-class CodeupPerimeterReFactor {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in).useDelimiter("\n");
-        System.out.println("Enter a length, width, and height:");
-        float userLength = s.nextFloat();
-        s.nextLine();
-        float userWidth = s.nextFloat();
-        s.nextLine();
-        float userHeight = s.nextFloat();
-        float area = userLength * userWidth;
-        float perimeter = (userLength * 2) + (userWidth * 2);
-        float volume = userLength * userHeight * userWidth;
-        System.out.printf("Area: %s ft\u00B2%nPerimeter: %s ft%nVolume: %s ft\u00B3%n ", area, perimeter, volume);
-        s.close();
-    }
-}
 
