a13df25b1939d463d76635b6ff7608346dfadf07
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-05 12:44:12 -0500

    RPG console game (working on player creation / class)

--- src/RPG.java
+++ src/RPG.java
@@ -1,35 +1,63 @@
 // RPG console game
+
 import java.util.Scanner;
 
 public class RPG {
     static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
-        gameStart();
+        mainMenu();
 
     }
-    public static void gameStart () {
+
+    public static void mainMenu() {
         System.out.println("Welcome to ScrimmRPG!");
         System.out.print("[START] [CLOSE] [HOW TO PLAY]:");
         String mainMenuUserResponse = s.nextLine();
-        if (mainMenuUserResponse.equals("start")) {
+        if (mainMenuUserResponse.equalsIgnoreCase("start")) {
             start();
-        } else if (mainMenuUserResponse.equals("close")) {
+        } else if (mainMenuUserResponse.equalsIgnoreCase("close")) {
             close();
         } else {
-            gameStart();
+            mainMenu();
         }
-
     }
 
-    public static void start () {
+    static String playerClass;
+    static String playerName;
+
+    public static void start() {
         System.out.println("Let's begin.");
         System.out.print("What is your name adventurer?:");
-        String playerName = s.nextLine();
-        int playerHealth = 100;
-        System.out.println();
+        playerName = s.nextLine();
+        if (playerName.matches("[a-zA-Z]+$")) {
+            classSelection();
+        } else {
+            System.out.println("Player name can only contain alphabetical characters a-Z.");
+            start();
+        }
     }
 
-    public static void close () {
+    public static void classSelection() {
+        int playerHealth;
+        System.out.print("What class are you " + playerName + "?\n[ROUGE (low DEF high ATK)]\n[MAGE (low DEF high ATK)]\n[ARCHER (high DEF low ATK)]\nClass selection:");
+        playerClass = s.nextLine();
+        switch (playerClass) {
+            case "rouge", "mage" -> {
+                playerHealth = 50;
+                System.out.println(playerName + " the " + playerClass + ", you have: " + playerHealth + "HP.");
+            }
+            case "archer" -> {
+                playerHealth = 100;
+                System.out.println(playerName + " the " + playerClass + ", you have: " + playerHealth + "HP.");
+            }
+            default -> {
+                System.out.println("Enter a valid class!");
+                classSelection();
+            }
+        }
+    }
+
+    public static void close() {
         System.out.println("Goodbye!");
     }
 }
