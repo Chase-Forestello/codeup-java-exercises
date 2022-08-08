34956f43bb1346577f04bf4494097dcb215ec492
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-08 17:00:52 -0500

    Complete - Java II - OOP Person Exercise

--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,22 @@
-// Incomplete - Java II - OOP Exercise
+// Complete - Java II - OOP Person Exercise
 public class Person {
-    private String name;
+    private static String name;
 
     public static void main(String[] args) {
         Person person = new Person("Chase");
-//        System.out.println(person.name);
         System.out.println(person.getName());
-        person.setName("notChase");
         person.sayHello();
+        person.setName("not Chase");
+        person.sayHello();
+        Person person5 = new Person("Someone");
+        System.out.println(person5.getName());
+        person5.sayHello();
+        person5.setName("Someone else");
+        person5.sayHello();
     }
 
-    public Person(String name) {
-        this.name = name;
+    private Person(String name) {
+        Person.name = name;
     }
 
     public String getName() {
@@ -19,11 +24,11 @@ public class Person {
     }
 
     public void setName(String name) {
-        this.name = name;
+        Person.name = name;
     }
 
     public void sayHello() {
-        System.out.println("Hello, " + this.name + "!");
+        System.out.println("Hello, " + name + "!");
 
     }
 }
