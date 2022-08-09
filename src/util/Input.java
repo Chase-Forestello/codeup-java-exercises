d68a36e6dfeb293c8f4096932770ff2638de8814
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-09 15:24:20 -0500

    Complete - Java II - OOP Input Exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,55 +1,89 @@
-// Incomplete - Java II - OOP Input Exercise
+// Complete - Java II - OOP Input Exercise
 package util;
+
 import java.util.Scanner;
 
 public class Input {
-    private Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
 
     public static void main(String[] args) {
         Input input = new Input();
-        System.out.println("Enter a word:");
-        System.out.println(input.getString());
-        System.out.println(input.yesNo());
-        System.out.println(input.getInt(1, 10));
+        System.out.println(input.getString("Enter a word:"));
+        System.out.println(input.yesNo("Enter 'yes' or 'y':"));
+        System.out.println(input.getInt(1, 10, "Enter an integer 1-10:"));
+        System.out.println(input.getInt("Enter an integer:"));
+        System.out.println(input.getDouble(1, 10, "Enter a double 1-10:"));
+        System.out.println(input.getDouble("Enter a double:"));
     }
 
 
-    // Returns the user input if it is a string
-    public String getString() {
+    // Returns next string
+    public String getString(String prompt) {
+        System.out.print(prompt);
         return scanner.nextLine();
     }
 
 
     // Returns true if the user input = "yes" or "y" else returns false.
-    public boolean yesNo() {
-        System.out.print("Enter 'yes' or 'y':");
-        String userInput = getString();
-        boolean passed;
-        passed = userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
-        return passed;
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        String userInput = scanner.nextLine();
+        return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
     }
 
 
-    // Return success or continue prompting the user until they are within range (1-10).
-    public String getInt(int min, int max) {
-        System.out.print("Enter a number 1-10:");
-        boolean userInput = scanner.hasNextInt();
-            int userIntInput;
-        if (userInput) {
-            userIntInput = scanner.nextInt();
-            if (min < userIntInput && userIntInput < max) {
-                System.out.println("Good job!");
+    // Returns int or continue prompting the user until they are within range (1-10).
+    public int getInt(int min, int max, String prompt) {
+        System.out.print(prompt);
+        boolean intRange;
+        int result;
+        do {
+            int userInt = scanner.nextInt();
+            result = userInt;
+            if (userInt > max || userInt < min) {
+                System.out.print("Enter int within 1-10 range: ");
             } else {
-                System.out.println("Try again!");
-                getInt(1,10);
+                System.out.print("Your int is: ");
             }
-        } else {
-            System.out.print("Try again:");
-            Input input = new Input();
-            input.getInt(1, 10);
-        }
-        return "Input is in range!";
+            intRange = (userInt > max || userInt < min);
+        } while (intRange);
+        return result;
+    }
+
+    // Returns next int
+    public int getInt(String prompt) {
+        System.out.print(prompt);
+//        System.out.print("Enter a radius: ");
+        return scanner.nextInt();
+    }
+
+    // Returns double or continue prompting the user until they are within range (1-10).
+    public double getDouble(double min, double max, String prompt) {
+        System.out.print(prompt);
+        boolean DoubleRange;
+        double result;
+        do {
+            double userDouble = scanner.nextDouble();
+            result = userDouble;
+            if (userDouble > max || userDouble < min) {
+                System.out.print(prompt);
+            } else {
+                System.out.print("Your double is: ");
+            }
+            DoubleRange = (userDouble > max || userDouble < min);
+        } while (DoubleRange);
+        return result;
+    }
+
+    // Returns next double
+    public double getDouble(String prompt) {
+        System.out.print(prompt);
+        double userInput = scanner.nextDouble();
+        // Clearing buffer from scanner
+        scanner.nextLine();
+        return userInput;
     }
 }
+
 
