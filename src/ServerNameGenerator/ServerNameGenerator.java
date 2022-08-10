140a6a6b8bd43a31c7de1bb2682c7294d57994dd
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-10 17:08:25 -0500

    Complete - Java II - Arrays Server Name Generator

---
+++ src/ServerNameGenerator/ServerNameGenerator.java
@@ -0,0 +1,22 @@
+// Complete - Java II - Arrays Server Name Generator
+package ServerNameGenerator;
+
+import java.util.Random;
+
+public class ServerNameGenerator {
+
+    public static void main(String[] args) {
+        String randomAdj = getRandomIndex(adjectives);
+        String randomNoun = getRandomIndex(nouns);
+        System.out.println("Here is your server name:\n" + randomAdj + "-" + randomNoun);
+    }
+
+    private static final String[] adjectives = {"tiny", "oval", "combative", "hateful", "awful", "safe", "premium", "nondescript", "royal", "delirious"};
+    private static final String[] nouns = {"editor", "instance", "security", "goal", "insurance", "perception", "meat", "possession", "championship", "obligation"};
+
+    public static String getRandomIndex(String[] array) {
+        int randomIndex = (int) (Math.random() * array.length);
+        return array[randomIndex];
+    }
+
+}
