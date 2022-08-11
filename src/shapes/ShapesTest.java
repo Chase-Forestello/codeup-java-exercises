047d545a49f857c6e1a3b1f2f4587456190c6552
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-11 17:06:03 -0500

    Incomplete - Java II - Interfaces and Abstract Classes Exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -5,7 +5,7 @@ package shapes;
 public class ShapesTest {
     public static void main(String[] args) {
 //        Rectangle box1 = new Rectangle(4,5);
-//        System.out.println(box1.perimeter());
+//        System.out.println(box1.Perimeter());
 //        System.out.println(box1.area());
 //        Rectangle box2 = new Square(2);
 //        System.out.println(box2.perimeter());
