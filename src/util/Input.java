6ab13e47136fba6eabcabc23c5737fd66eddbc5d
Chase Forestello <101914667+Chase-Forestello@users.noreply.github.com>
2022-08-16 16:05:04 -0500

    Complete - Java II - Exceptions and Error Handling (CLI could be improved)

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,3 +1,4 @@
+// Complete - Java II - Exceptions and Error Handling (CLI could be improved)
 // Complete - Java II - OOP Input Exercise
 package util;
 
@@ -9,12 +10,14 @@ public class Input {
 
     public static void main(String[] args) {
         Input input = new Input();
-        System.out.println(input.getString("Enter a word:"));
-        System.out.println(input.yesNo("Enter 'yes' or 'y':"));
+//        System.out.println(input.getString("Enter a word:"));
+//        System.out.println(input.yesNo("Enter 'yes' or 'y':"));
         System.out.println(input.getInt(1, 10, "Enter an integer 1-10:"));
         System.out.println(input.getInt("Enter an integer:"));
         System.out.println(input.getDouble(1, 10, "Enter a double 1-10:"));
         System.out.println(input.getDouble("Enter a double:"));
+        System.out.println(input.getBinary("Enter a binary number:"));
+        System.out.println(input.getHex("Enter a hexadecimal:"));
     }
 
 
@@ -22,6 +25,11 @@ public class Input {
     public String getString(String prompt) {
         System.out.print(prompt);
         return scanner.nextLine();
+    }
+
+    public String getString() {
+        String s = scanner.nextLine();
+        return s;
     }
 
 
@@ -37,53 +45,108 @@ public class Input {
     public int getInt(int min, int max, String prompt) {
         System.out.print(prompt);
         boolean intRange;
-        int result;
+        int userInt;
         do {
-            int userInt = scanner.nextInt();
-            result = userInt;
+            userInt = 0;
+            try {
+                userInt = Integer.valueOf(getString());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+                System.out.println(e.getMessage());
+            }
             if (userInt > max || userInt < min) {
                 System.out.print("Enter int within 1-10 range: ");
-            } else {
-                System.out.print("Your int is: ");
             }
             intRange = (userInt > max || userInt < min);
         } while (intRange);
-        return result;
+        return userInt;
     }
 
     // Returns next int
     public int getInt(String prompt) {
-        System.out.print(prompt);
-//        System.out.print("Enter a radius: ");
-        return scanner.nextInt();
+        while (true) {
+            System.out.print(prompt);
+            try {
+                int userInput = Integer.valueOf(getString());
+                System.out.print("Valid input: ");
+                return userInput;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     // Returns double or continue prompting the user until they are within range (1-10).
+    // How to set up if statement to check for 1-10 while checking for exceptions?
     public double getDouble(double min, double max, String prompt) {
-        System.out.print(prompt);
-        boolean DoubleRange;
-        double result;
-        do {
-            double userDouble = scanner.nextDouble();
-            result = userDouble;
-            if (userDouble > max || userDouble < min) {
-                System.out.print(prompt);
-            } else {
-                System.out.print("Your double is: ");
-            }
-            DoubleRange = (userDouble > max || userDouble < min);
-        } while (DoubleRange);
-        return result;
+        while (true) {
+            System.out.print(prompt);
+            boolean DoubleRange;
+            double userDouble;
+            do {
+                userDouble = 0;
+                try {
+                    userDouble = Double.valueOf(getString());
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input");
+                    System.out.println(e.getMessage());
+                }
+                if (userDouble > max || userDouble < min) {
+                    System.out.print("Enter double within 1-10 range: ");
+                }
+                DoubleRange = (userDouble > max || userDouble < min);
+            } while (DoubleRange);
+            return userDouble;
+        }
     }
 
     // Returns next double
     public double getDouble(String prompt) {
-        System.out.print(prompt);
-        double userInput = scanner.nextDouble();
-        // Clearing buffer from scanner
-        scanner.nextLine();
-        return userInput;
+        while (true) {
+            System.out.print(prompt);
+            try {
+                double userInput = Double.valueOf(getString());
+                System.out.println("Valid input: ");
+                return userInput;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+                System.out.println(e.getMessage());
+            }
+        }
     }
+
+    public int getBinary(String prompt) {
+        int userInput;
+        while (true) {
+            System.out.print(prompt);
+            try {
+                userInput = Integer.valueOf(getString(), 2);
+                System.out.println("Valid input: ");
+                return userInput;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input");
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public long getHex(String prompt) {
+        int userInput;
+        while (true) {
+            System.out.print(prompt);
+            try {
+                userInput = Integer.valueOf(getString(), 16);
+                System.out.println("Valid input: ");
+                return userInput;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input: ");
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
 }
 
 
